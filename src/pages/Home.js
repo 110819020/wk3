17c53e22c9348d1cfcf28ba64c7d2c76a0a8ed2f
SonaruIsuge <bread511206@gmail.com>
2021-03-12 20:56:27 +0800

    grid layout RWD

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import ProductList from "../components/ProductList";
+import NavBar from "../components/NavBar";
 
 function Home() {
   return (
     <div className="bg-color">
         <div className="container grid-container">
             <Header />
+            <NavBar />
             <ProductList />
             <Footer />
         </div>
